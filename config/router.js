19a6c68c1a32bf0f95ec32222da3c0deb0641b9a
jcaruana09 <jcaruana09@outlook.com>
2018-05-04 15:59:02 +0100

    created crawls controller and updated the router.js

--- config/router.js
+++ config/router.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
+const crawls = require('../controllers/crawls');
+
+router.route('/crawls')
+  .post(crawls.create);
+
+router.route('/crawls/:id')
+  .get(crawls.show)
+  .put(crawls.update)
+  .delete(crawls.delete);
 
 module.exports = router;
