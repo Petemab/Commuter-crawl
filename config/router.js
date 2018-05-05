05a6d6702a88fbb7d0aecec60c50b62fb7d05b4d
leon Boakye-Adjei <leon_adjei@hotmail.com>
2018-05-05 13:54:48 +0100

    updated router for auth

--- config/router.js
+++ config/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const crawls = require('../controllers/crawls');
+const auth = require('../controllers/auth');
 
 router.route('/crawls')
   .post(crawls.create);
@@ -8,5 +9,8 @@ router.route('/crawls/:id')
   .get(crawls.show)
   .put(crawls.update)
   .delete(crawls.delete);
+
+router.post('/register', auth.register);
+router.post('/login', auth.login);
 
 module.exports = router;
