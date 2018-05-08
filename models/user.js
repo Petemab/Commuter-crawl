c875f7a01a3e9c7a96fe2277be27172325d5918c
leon Boakye-Adjei <leon_adjei@hotmail.com>
2018-05-08 11:32:38 +0100

    fix err bug

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 mongoose.Promise = require('bluebird');
 
-//Declaring userSchema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, unique: true },
@@ -23,25 +22,25 @@ userSchema.methods.validatePassword = function validatePassword(password){
 };
 
 userSchema
-  .virtual('passwordconfirmation')
+  .virtual('passwordConfirmation')
   .set(function setPasswordConfirmation(passwordConfirmation){
     this._passwordConfirmation = passwordConfirmation;
   });
 
-
+//Before (pre) any function 'saves' something, run this function to encrypt the password before it is stored:
 userSchema.pre('validate', function checkPassword(next){
   if(!this.password) {
     this.invalidate('password', 'password is required');
   }
-  if(this.isModified('password') && this._passordCofirmation !== this.password){
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
     this.invalidate('passwordConfirmation', 'does not match');
   }
   next();
 });
 
-userSchema.pre('save', function hashPasword(next){
+userSchema.pre('save', function hashPassword(next){
   if(this.isModified('password')){
-    this.password = bcrypt.hashSync(this.pasword, bcrypt.genSaltSync(8));
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
   }
   next();
 });
