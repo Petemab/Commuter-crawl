862fe56d88f286844bd29607b66012c6ee1d6ebf
jcaruana09 <jcaruana09@outlook.com>
2018-05-11 13:58:27 +0100

    Setting up for deployment

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const { port, dbURI } = require('./config/environment');
 const errorHandler = require('./lib/errorHandler');
 
 mongoose.connect(dbURI);
-
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use('/api', router);
-
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Up and running on port ${port}`));
